5b41bb506dab914a18130d4b24d24095a3ef3919
Matheus.Aguirra <65245102+matheusaaguirra@users.noreply.github.com>
2024-04-18 12:08:23 -0300

    projeto de toca som
    Projeto inacabado, estado de produção

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const IdAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(IdAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        
+        if(evento.code === 'keya'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeydown = function() {
+        tecla.classList.remove ('ativa');
+    }
+} 
